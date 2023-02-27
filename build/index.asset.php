2119cd5d1e76d414324c3834645ba71294f21b17
David Heckel <dheckel77@gmail.com>
2023-02-27 16:43:37 -0500

    Init Commit

---
+++ build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-data', 'wp-element'), 'version' => 'bfd3d834004f3451da8f');
---
+++ featured-professor.php
@@ -0,0 +1,74 @@
+<?php
+
+/*
+  Plugin Name: Featured Professor Gutenberg Block 
+  Description: Adds a Featured Professor block type to the Gutenberg menu options
+  Version: 1.1
+  Author: David Heckel
+  Author URI: https://davidheckel.dev
+  Text Domain: featured-professor
+  Domain Path: /languages
+*/
+
+if( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+
+require_once plugin_dir_path( __FILE__ ) . 'inc/generateProfessorHTML.php';
+require_once plugin_dir_path( __FILE__ ) . 'inc/relatedPostsHTML.php';
+
+class FeaturedProfessor {
+  function __construct() {
+    add_action('init', [$this, 'onInit']);
+    add_action( 'rest_api_init', [$this, 'profHTML'] );
+    add_filter('the_content', [$this, 'addRelatedPosts']);
+  }
+
+  function addRelatedPosts($content) {
+    if (is_singular('professor') && in_the_loop() && is_main_query()) {
+      return $content . relatedPostsHTML(get_the_id());
+    }
+    return $content;
+  }
+
+  function profHTML() {
+    register_rest_route('featuredProfessor/v1', 'getHTML', array(
+      'methods' => WP_REST_SERVER::READABLE,
+      'callback' => [$this, 'getProfHTML']
+    ));
+  }
+
+  function getProfHTML($data) {
+    return generateProfessorHTML($data['profID']);
+  }
+
+  function onInit() {
+    load_plugin_textdomain('featured-professor', false, dirname(plugin_basename(__FILE__)) . '/languages');
+
+    register_meta('post', 'featuredprofessor', array(
+      'show_in_rest' => true,
+      'type' => 'number',
+      'single' => false
+    ));
+    wp_register_script('featuredProfessorScript', plugin_dir_url(__FILE__) . 'build/index.js', array('wp-blocks', 'wp-i18n', 'wp-editor'));
+    wp_register_style('featuredProfessorStyle', plugin_dir_url(__FILE__) . 'build/index.css');
+
+    wp_set_script_translations('featuredProfessorScript', 'featured-professor', plugin_dir_path(__FILE__) . '/languages' );
+
+    register_block_type('ourplugin/featured-professor', array(
+      'render_callback' => [$this, 'renderCallback'],
+      'editor_script' => 'featuredProfessorScript',
+      'editor_style' => 'featuredProfessorStyle'
+    ));
+  }
+
+  function renderCallback($attributes) {
+    if ($attributes['profID']) {
+      wp_enqueue_style('featuredProfessorStyle');
+      return generateProfessorHTML($attributes['profID']);
+      } else {
+      return NULL;
+    }
+  }
+
+}
+
+$featuredProfessor = new FeaturedProfessor();
